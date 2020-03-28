dd273dc58d36dbd86aa4115e3f7c1115e39ad81f
Ahmed Elhegery <ahmed.3laa.2020@gmail.com>
2020-03-28 15:00:35 +0200

    step-1-create-vue-component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Input Component</h1>
+        <input type="text">
+    </div>
+</template>
+
+<script>
+export default {
+    name : "SearchInput"
+    
+}
+</script>
+
+<style>
+
+</style>
